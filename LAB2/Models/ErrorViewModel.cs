fa8672597f77e3f177c68999756ce1a70da82951
Seniv Volodymyr <01156240@pw.edu.pl>
2023-07-11 00:43:36 +0200

    Adding projects from EOPSY course.

--- LAB2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BlogProject22.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
